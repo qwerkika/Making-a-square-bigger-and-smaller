57bbcf08271690b58347567ab367dbdcaef18650
qwerkika <130110236+qwerkika@users.noreply.github.com>
2023-12-07 19:38:07 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
+function setup()
+{
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+function modelLoaded()
+{
+    console.log("poseNet is initilized");
+}
+function draw()
+{
+    background("#89DAFF");
+    document.getElementById("square_side").innerHTML="Width and the Height of the square will be="+difference+"pX"
+    fill("#f90093");
+    stroke("#f90093");
+    square(noseX,noseY,difference);
+}
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX="+noseX+"noseY="+noseY);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftWristX="+leftWristX+"rightWristX="+rightWristX+"difference="+difference);
+        
+    }
+}
